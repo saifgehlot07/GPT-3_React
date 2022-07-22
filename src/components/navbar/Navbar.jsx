6b4356c8ea3e60c8141462be5c8adce440e28060
saifgehlot0 <gehlotsaif07@gmail.com>
2022-07-22 22:31:04 +0530

    initial commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+// import { RiMenu3Lin, RiCloseLine } from "./react-icons/ri";
 import "./nav.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className=""></div>;
 };
 
 export default Navbar;
