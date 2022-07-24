352975333b464ddb0e4490dee63bd062730e4110
saifgehlot0 <gehlotsaif07@gmail.com>
2022-07-24 23:16:15 +0530

    initial commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 // import { RiMenu3Lin, RiCloseLine } from "./react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./nav.css";
 
 const Navbar = () => {
-  return <div className=""></div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links__container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#home">What is GPT?</a>
+          </p>
+          <p>
+            <a href="#home">Open Ai</a>
+          </p>
+          <p>
+            <a href="#home">Case Studio</a>
+          </p>
+          <p>
+            <a href="#home">Library</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
